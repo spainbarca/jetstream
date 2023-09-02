5c62b65ed8c8878c5eca90d1cce6b7d88a605b4d
Noah <spain.barcelona.1999@gmail.com>
2023-09-02 17:49:58 -0500

    navegando por los blades de jetstream

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,20 +1,32 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+@php
+    $nav_links= [
+        [
+            'name' => 'Home',
+            'route' => route('home'),
+            'active' => request()->routeIs('home'),
+        ]
+    ];
+@endphp
+
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 shadow">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('home') }}">
                         <x-application-mark class="block h-9 w-auto" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+                    @foreach ($nav_links as $nav_link)
+                        <x-nav-link href="{{ $nav_link['route'] }}" :active="$nav_link['active']">
+                            {{ $nav_link['name'] }}
+                        </x-nav-link>
+                    @endforeach
                 </div>
             </div>
 
@@ -73,6 +85,7 @@
 
                 <!-- Settings Dropdown -->
                 <div class="ml-3 relative">
+                    @auth
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
@@ -121,6 +134,7 @@
                             </form>
                         </x-slot>
                     </x-dropdown>
+                    @endauth
                 </div>
             </div>
 
@@ -139,9 +153,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
+            @foreach ($nav_links as $nav_link)
+                <x-responsive-nav-link href="{{ $nav_link['route'] }}" :active="$nav_link['active']">
+                    {{ $nav_link['name'] }}
+                </x-responsive-nav-link>
+            @endforeach
         </div>
 
         <!-- Responsive Settings Options -->
